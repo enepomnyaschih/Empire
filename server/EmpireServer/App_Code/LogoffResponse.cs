5fc3a4bc5649856e8723374b21a6c61451dcf043
unknown <Pastushenko Ivan@.(none)>
2010-03-10 22:12:35 +0600

    new file:   EmpireServer/App_Code/GetInfoResponse.cs 	new file:   EmpireServer/App_Code/LoginResponse.cs 	new file:   EmpireServer/App_Code/LogoffResponse.cs 	new file:   EmpireServer/App_Code/RegisterResponse.cs 	new file:   EmpireServer/EmpireServer.csproj 	new file:   EmpireServer/EmpireServer.csproj.user 	new file:   EmpireServer/EmpireServer.sln 	new file:   EmpireServer/Properties/AssemblyInfo.cs 	new file:   EmpireServer/ServiceEmpire.ashx 	new file:   EmpireServer/ServiceEmpire.ashx.cs 	new file:   EmpireServer/Web.config

---
+++ server/EmpireServer/App_Code/LogoffResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace EmpireServer
+{
+    public abstract class LogoffResponse
+    {
+    }
+
+    [Serializable]
+    public class LogoffResponseTrue : LogoffResponse
+    {
+        public bool success;
+        public LogoffResponseTrue(bool vsuccess)
+        {
+            success = vsuccess;
+        }
+    }
+
+    public class LogoffResponseFalse : LogoffResponse
+    {
+        public bool success;
+        public string errorID;
+        public LogoffResponseFalse(bool vsuccess, string verrorID)
+        {
+            success = vsuccess;
+            errorID = verrorID;
+        }
+    }
+}
---
+++ server/EmpireServer/ServiceEmpire.ashx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.Xml.Linq;
+using Jayrock;
+using Jayrock.Json;
+using Jayrock.JsonRpc;
+using Jayrock.JsonRpc.Web;
+
+
+namespace EmpireServer
+{
+    [JsonRpcService]
+    [ToolboxItem(false)]
+  
+    public class ServiceEmpire : JsonRpcHandler
+    {
+        [JsonRpcMethod]
+        public object LoginRequest(string login, string password, bool remember)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable["success"] = true;
+            hashtable["ticket"] = 123456;
+            
+            return hashtable;
+        }
+
+        [JsonRpcMethod]
+        public object GetMemberInfo(int ticket)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable["success"] = true;
+            hashtable["memberID"] = "qwertqqwes";
+
+            return hashtable;
+        }
+
+        [JsonRpcMethod]
+        public object LogoffRequest(int ticket)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable["success"] = true;
+            
+            return hashtable;
+        }
+
+       [JsonRpcMethod]
+        public object RegisterRequest(string memberID, string password)
+        {
+            Hashtable hashtable = new Hashtable();
+
+            hashtable["success"] = true;
+            hashtable["errorID"] = "qwertqqwes";
+
+            return hashtable;
+        }
+
+    
+    }
+}
